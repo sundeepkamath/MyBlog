0a3685aa27918069f75b49f34ae0e48715cde90d
sundeep kamath <sundeep.g.kamath@gmail.com>
2015-08-23 02:13:02 +0530

    Added a javascript method to convert the post added date from UTC to local time in the clients browser. Also added the latest jquery libraries.

---
+++ MyBlog/Scripts/utils.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+    // Determine timezone offset in milliseconds
+    // from: http://www.w3schools.com/jsref/jsref_getTimezoneOffset.asp
+    alert("Hi");
+    var d = new Date()
+    var offsetms = d.getTimezoneOffset() * 60 * 1000;
+    $('.posted-date').each(function () {
+        try {
+            var text = $(this).html();
+
+            var n = new Date(text);
+            n = new Date(n.valueOf() - offsetms);
+
+            $(this).html(n.toDateString() + " " + n.toLocaleTimeString());
+
+            //$(this).attr("Converted from UTC " + text);
+        }
+        catch (ex) {
+            console.warn("Error converting time", ex);
+        }
+    });
+});
