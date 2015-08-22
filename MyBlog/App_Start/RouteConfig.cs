630245976cfa5dce2bd8b36224836fba9617dc83
sundeep kamath <sundeep.g.kamath@gmail.com>
2015-08-22 22:43:37 +0530

    Route added for blog in the route config

--- MyBlog/App_Start/RouteConfig.cs
+++ MyBlog/App_Start/RouteConfig.cs
@@ -14,10 +14,10 @@ namespace MyBlog
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                            "Action",
+                            "{action}",
+                            new { controller = "Blog", action = "Posts" }
+                          );
         }
     }
 }
