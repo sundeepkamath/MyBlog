8b9c69fae53ef0a997813487ea12ed0ac8d69bfe
sundeep kamath <sundeep.g.kamath@gmail.com>
2015-08-22 07:59:39 +0530

    Blog repository created

---
+++ MyBlog.Core/Repositories/BlogRepository.cs
@@ -0,0 +1,18 @@
+﻿using MyBlog.Core.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyBlog.Core.Entities;
+
+namespace MyBlog.Core.Repositories
+{
+    public class BlogRepository : IBlogRepository
+    {
+        public IQueryable<Post> GetPosts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyBlog.Core/RepositoryInterfaces/IBlogRepository.cs
@@ -0,0 +1,15 @@
+﻿using MyBlog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Core.RepositoryInterfaces
+{
+    public interface IBlogRepository
+    {
+        IQueryable<Post> GetPosts();
+
+    }
+}
