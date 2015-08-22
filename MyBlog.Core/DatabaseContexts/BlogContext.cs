10acd1762d3b2496f3b895b57fade2d24a812540
sundeep kamath <sundeep.g.kamath@gmail.com>
2015-08-22 07:57:04 +0530

    Database Context added for Blog

---
+++ MyBlog.Core/DatabaseContexts/BlogContext.cs
@@ -0,0 +1,22 @@
+﻿using MyBlog.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Core.DatabaseContexts
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext() : base("name=DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Category> Categories{ get; set; }
+    }
+}
