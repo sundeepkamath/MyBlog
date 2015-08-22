8295b26a14bd876ed3dfc78ef48aa958cb33d2c5
sundeep kamath <sundeep.g.kamath@gmail.com>
2015-08-22 07:47:40 +0530

    Added new entities for the Blog in the data layer

---
+++ MyBlog.Core/Entities/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBlog.Core.Entities
+{
+    public class Post   
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Description { get; set; }
+        public string Meta { get; set; }
+        public string UrlSlug { get; set; }
+        public bool Published { get; set; }
+        public DateTime PostedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+        public Category Category{ get; set; }
+        public ICollection<Tag> Tags { get; set; }
+
+    }
+}
